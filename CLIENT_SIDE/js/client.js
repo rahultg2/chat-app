a965a583ed7a4d4d4eed984fb6aa174844f8208c
Rahul <rsoni953804@gmail.com>
2021-06-14 17:16:24 +0530

    modified script tag and given path to the domain name

--- CLIENT_SIDE/js/client.js
+++ CLIENT_SIDE/js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000', { transports: ["websocket"] });
+const socket = io('https://chat-2-point-0.herokuapp.com:8000', { transports: ["websocket"] });
 
 // Geting DOM Element
 const form = document.getElementById('send_content');
