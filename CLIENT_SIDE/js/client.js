989219cf8ad5beead749e56029c0e4377465bcdd
Rahul <rsoni953804@gmail.com>
2021-06-15 00:18:08 +0530

    Added some port stuff

--- CLIENT_SIDE/js/client.js
+++ CLIENT_SIDE/js/client.js
@@ -1,5 +1,5 @@
 // const socket = io('https://chat-2-point-0.herokuapp.com', { transports: ["websocket"] });
-const socket = io('https://chat-2-point-0.herokuapp.com:8282');
+const socket = io('https://chat-2-point-0.herokuapp.com:8282', { transports: ["websocket"] });
 
 // Geting DOM Element
 const form = document.getElementById('send_content');
